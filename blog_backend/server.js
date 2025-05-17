3dca9402a9815a7add7dc9cbf3ab0d7ddc1d126b
singhyashe15 <singhyashraj574@gmail.com>
2025-05-17 17:19:53 +0530

    cors added

--- blog_backend/server.js
+++ blog_backend/server.js
@@ -4,10 +4,23 @@ import userTable from "./models/user.js";
 import blogSchema from './models/blogSchema.js'
 import authRouter from "./routes/auth.js";
 import blogRouter from "./routes/blogroute.js";
+import cors from "cors";
+
 const app = express();
 
 // define port
 const port = 3000 ;
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow necessary methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
+  optionsSuccessStatus: 200
+}));
+
+app.use(express.json());
+
 
 // connect with dbs
 pool.connect()
