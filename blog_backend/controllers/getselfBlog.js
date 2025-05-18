c320d652facfd7a5788b2048f99763111ab9bd34
singhyashe15 <singhyashraj574@gmail.com>
2025-05-18 12:51:30 +0530

    modified

--- blog_backend/controllers/getselfBlog.js
+++ blog_backend/controllers/getselfBlog.js
@@ -3,9 +3,9 @@ import pool from "../config/db.js"
 const getselfBlog = async(req,res)=>{
   try {
     const userId = req.params.id;
-    const allPublishedBlog = await pool.query("SELECT id,title,content,tag,status FROM BLOG WHERE userId=$1 AND status='published'",[userId]);
-    const allDraftBlog = await pool.query("SELECT id,title,content,tag,status FROM BLOG WHERE userId=$1 AND status='draft'",[userId])
-    return res.status(200).json({allPublishedBlog,allDraftBlog,success:true});
+    const allPublishedBlog = await pool.query("SELECT id,title,content,tag,status,updated_at FROM BLOG WHERE userId=$1 AND status='published'",[userId]);
+    const allDraftBlog = await pool.query("SELECT id,title,content,tag,status,updated_at FROM BLOG WHERE userId=$1 AND status='draft'",[userId])
+    return res.status(200).json({post : {allPublishedBlog,allDraftBlog},success:true});
   } catch (error) {
     return res.status(501).json({error});
   }
