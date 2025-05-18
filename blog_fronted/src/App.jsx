4977e222e8cf8cebd204905b762d2f2ab7ab140c
singhyashe15 <singhyashraj574@gmail.com>
2025-05-18 15:16:17 +0530

    modified

--- blog_fronted/src/App.jsx
+++ blog_fronted/src/App.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
 import { HStack, Text, Input, Icon, IconButton, Menu, MenuItem, MenuButton, MenuList, Button } from '@chakra-ui/react';
-import { FaEdit, FaUserCircle } from 'react-icons/fa';
-import { Outlet, useNavigate } from 'react-router-dom';
-import { Toaster,toast } from 'react-hot-toast';
+import { FaEdit, FaLock, FaSignInAlt, FaUser, FaUserCircle } from 'react-icons/fa';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Toaster, toast } from 'react-hot-toast';
 import { useBlog } from "./context/blogcontext.jsx";
-import './App.css'
+import './App.css';
 
 function App() {
-  const { search,setSearch } = useBlog();
+  const { search, setSearch } = useBlog();
   const [isExist, setIsExist] = useState(false);
+  const location = useLocation()
   const navigate = useNavigate()
   let user = null;
 
@@ -21,14 +22,14 @@ function App() {
   }, []);
 
   //search by category 
-  const handleChange = (e) =>{
-    const {value} = e.target;
+  const handleChange = (e) => {
+    const { value } = e.target;
     setSearch(value);
   }
 
   // write a post
   const handlePost = () => {
-    if (!user) {
+    if (user) {
       navigate('/blog-post', { replace: true })
     } else {
       toast("Login First");
@@ -37,34 +38,59 @@ function App() {
 
   return (
     <>
-    {/* navbar */}
-      <HStack w="100%" justify="space-between" px={{base:"4" , md:"0"}}  >
+      {/* navbar */}
+      <HStack w="100%" justify="space-between" px={{ base: "4", md: "0" }}>
         {/* Left Side */}
         <HStack align="start" spacing="3">
           <Text fontSize="2xl" fontWeight="bold">Blog</Text>
-          <Input placeholder="Search for category" value={search} size="sm" rounded="full" onChange={handleChange} />
+          {!['/dashboard', '/profile', '/logout', '/login', '/register'].includes(location.pathname) && (
+            <Input
+              placeholder="Search for category"
+              value={search}
+              size="sm"
+              rounded="full"
+              onChange={handleChange}
+            />
+          )}
         </HStack>
 
         {/* Right Side */}
         <HStack align="end" spacing="4">
-          <IconButton aria-label="Search" onClick={() => handlePost()} title="Write a Post">
-            <Icon as={FaEdit} alt="write" />
-          </IconButton>
-
-          {!isExist ? (
+          {!['/login', '/register', '/logout'].includes(location.pathname) && (
+            <IconButton
+              aria-label="Write Post"
+              icon={<FaEdit />}
+              title="Write a Post"
+              onClick={handlePost}
+            />
+          )
+          }
+          {isExist ? (
             <Menu>
               <MenuButton as={Button} rightIcon={<FaUserCircle />}>
+                {/* You can put username here if needed */}
               </MenuButton>
               <MenuList>
-                <MenuItem onClick={() => navigate('/profile')}>Dashboard</MenuItem>
-                <MenuItem>Settings</MenuItem>
-                <MenuItem color="red.500" onClick={() => navigate('/logout', { replace: true })}>Sign Out</MenuItem>
+                <MenuItem onClick={() => navigate('/dashboard')}>
+                  <Icon as={FaUser} mr={2} /> Dashboard
+                </MenuItem>
+                <MenuItem onClick={() => navigate('/profile')}>
+                  <Icon as={FaUserCircle} mr={2} /> Edit Profile
+                </MenuItem>
+                <MenuItem onClick={() => navigate('/change-password')}>
+                  <Icon as={FaLock} mr={2} /> Change Password
+                </MenuItem>
+                <MenuItem color="red.500" onClick={() => navigate('/logout', { replace: true })}>
+                  <Icon as={FaSignInAlt} mr={2} /> Sign Out
+                </MenuItem>
               </MenuList>
             </Menu>
           ) : (
-            <Button m="4" colorScheme="teal">
-              LoginIn
-            </Button>
+            !['/login', '/register'].includes(location.pathname) && (
+              <Button colorScheme="teal" onClick={() => navigate('/login')}>
+                Login
+              </Button>
+            )
           )}
         </HStack>
       </HStack>
