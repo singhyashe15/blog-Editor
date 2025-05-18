eedaf83197be4034cacac49433866c362aef6248
singhyashe15 <singhyashraj574@gmail.com>
2025-05-18 16:21:27 +0530

    change user

--- blog_backend/models/blogSchema.js
+++ blog_backend/models/blogSchema.js
@@ -10,7 +10,7 @@ const BlogSchema = async () => {
       tags JSONB , 
       status VARCHAR(255) CHECK (status IN ('draft', 'published')),
       imageUrl VARCHAR(255),
-      userId INTEGER REFERENCES user(id),
+      userId INTEGER REFERENCES users(id),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
