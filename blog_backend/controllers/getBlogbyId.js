9c03e0dc0e6c2c370932db41c603c0e90d883e8c
singhyashe15 <singhyashraj574@gmail.com>
2025-05-18 16:05:12 +0530

    read blog modified

--- blog_backend/controllers/getBlogbyId.js
+++ blog_backend/controllers/getBlogbyId.js
@@ -1,16 +1,18 @@
 import pool from "../config/db.js";
 
-const getBlog = async(req,res) =>{
+const getBlog = async (req, res) => {
   try {
-    const {id} = await req.params;
+    const { id } = await req.params;
 
-   const blog =  await pool.query("SELECT id,title,content,tags,status FROM BLOG WHERE id=$1", [id]);
+    const blog = await pool.query("SELECT id,title,content,tags,status FROM BLOG WHERE id=$1", [id]);
 
-   if(blog.rowCount == 0){
-    return res.status(301).json({msg:"Blogs not Founded", success: false});
-   }
-
-   return res.status(201).json({blog,success:true});
+    if (blog.rowCount == 0) {
+      return res.status(301).json({ msg: "Blogs not Founded", success: false });
+    }
+    // fetch the user who has published that blog
+    const user = await pool.query("SELECT name FROM USER WHERE id=$1", [blog.rows[0].userId]);
+    
+    return res.status(201).json({ post: { user, blog }, success: true });
   } catch (error) {
     return res.status(501).json(error);
   }
--- blog_fronted/src/controllers/readblog.jsx
+++ blog_fronted/src/controllers/readblog.jsx
@@ -1,10 +1,68 @@
+import { Heading, Image, Stack, Text, Box, Flex, Wrap, WrapItem, Tag, TagLabel } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { useParams } from "react-router-dom"
 
-export default function readBlog(){
-  const {id} = useParams();
+export default function ReadBlog() {
+  const { id } = useParams();
 
-  
+  // get the post by id
+  const getBlogById = async () => {
+    console.log(id)
+    const url = import.meta.env.VITE_SERVER_URL;
+    const res = await axios.get(`${url}/blog/${id}`);
+    console.log(res.data);
+    return res.data.success ? res.data.post : [];
+  }
+  const { data } = useQuery({
+    queryKey: "blogId",
+    queryFn: getBlogById(),
+    staleTime: 1000
+  })
+
   return (
-    <></>
+    <Stack spacing={6} direction="column" p={6} mx="8" mt={8} shadow="lg" rounded="xl" bg="white">
+      {/*BLog Image */}
+      <Box w="100%" h="250px" overflow="hidden" rounded="md">
+        <Image
+          src=""
+          alt="Blog cover"
+          objectFit="cover"
+          w="100%"
+          h="100%"
+        />
+      </Box>
+
+      {/* Author and Date */}
+      <Flex justify="space-between" align="center" color="gray.600" fontSize="sm">
+        <Text fontWeight="medium">Ravi Sharma</Text>
+        <Text>Published on: 28-05-2025</Text>
+      </Flex>
+
+      {/* Tags */}
+      <Wrap mt={2}>
+        {["computer", "ai"]?.map((tag) => (
+          <WrapItem key={tag}>
+            <Tag
+              size="md"
+              variant="solid"
+              borderRadius="lg"
+              padding="4"
+            >
+              <TagLabel>{tag}</TagLabel>
+            </Tag>
+          </WrapItem>
+        ))}
+      </Wrap>
+      {/* Content */}
+
+      <Heading size="md" mb={2} float="left">Artificial Intelligence</Heading>
+      <Text float="left">
+        How AI is capturing the minds of young youth around the world, influencing decisions,
+        creativity, and productivity like never before. i am yashraj singh b.teh of academy of technology
+      </Text>
+
+    </Stack>
+
   )
 }
