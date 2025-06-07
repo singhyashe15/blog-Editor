bb2f3b6e1d5a568181a8717d1ba6d43c9283968e
singhyashe15 <singhyashraj574@gmail.com>
2025-06-08 00:34:38 +0530

    commited

--- blog_fronted/src/controllers/readblog.jsx
+++ blog_fronted/src/controllers/readblog.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom"
 import moment from "moment"
 import { useRef, useState } from "react";
-import { FaPaperPlane, FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+import { FaPaperPlane, FaThumbsUp, FaThumbsDown, FaHeart, FaLightbulb, FaLaugh } from "react-icons/fa";
 import { useSocket } from "../context/socket.jsx";
 import { useEffect } from "react";
 export default function ReadBlog() {
@@ -12,8 +12,8 @@ export default function ReadBlog() {
   const [profile, setProfile] = useState("");
   const [comment, setComment] = useState("");
   const [allComments, setAllComments] = useState([]);
-  const [liked,setLiked] = useState(-1);
-  const [unliked,setunLiked] = useState(-1);
+  const [icon, setIcon] = useState(null);
+  const [toggle, setToggle] = useState(-1);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const socket = useSocket();
   const btnRef = useRef()
@@ -43,35 +43,26 @@ export default function ReadBlog() {
 
   // socket logic
   useEffect(() => {
-    console.log("eo")
+
     if (socket) {
-      console.log(socket.connected)
       socket.emit('room', id); // using blog.id as a room for real time communication
       // getting all the comment
-      socket.emit('send-comment',id);
+      socket.emit('send-comment', id);
       // getting the broadcasted comment
       socket.on('all-comment', (comment) => {
         setAllComments(comment)
+        console.log(allComments)
       });
 
       // if(liked !== -1){
       //   socket.emit('setLike',)
       // }
     }
-  }, [socket,id])
-
-  // handling the comment
-  const handleComment = (e) => {
-    const { value } = e.target;
-
-    setComment(value)
-
-  }
+  }, [socket, id])
 
   const submitComment = async () => {
     //  sending the comment in real time
     if (comment && socket.connected) {
-      console.log("enter")
       socket.emit('comment', {
         roomId: id,
         sender: profile.name,
@@ -84,12 +75,15 @@ export default function ReadBlog() {
   }
 
   const handleKey = (e) => {
-    if(e.key === 'Enter'){
-      console.log("enter")
+    if (e.key === 'Enter') {
       submitComment();
     }
   }
 
+  const handleIcon = (icon) => {
+    setIcon(icon);
+    setToggle(toggle*10);
+  }
 
   return (
     <Stack spacing={6} direction="column" p={6} mx="8" mt={8} shadow="lg" rounded="xl" bg="white">
@@ -136,7 +130,6 @@ export default function ReadBlog() {
       <Text float="left">
         {data?.blog?.rows[0].content}
       </Text>
-      {/* } */}
 
       {/* For Comment Section */}
       <Drawer
@@ -155,48 +148,60 @@ export default function ReadBlog() {
             {
               allComments.map((comment) => {
                 return (
-                  <Flex key={comment.id} mt='4' justify="flex-end" align="flex-start" ml={comment.sender_id === profile.id ? '26' : 'auto'}>
-                    <Avatar size="sm" cursor="pointer" mt="4" mr="2"></Avatar>
+                  <Flex key={comment.id} mt='4' p="2" justify={comment.sender_id === Number(profile.id) && "flex-end"} position="relative" >
+                    <Avatar size="sm" cursor="pointer" mt="4" mr="2" ></Avatar>
                     <Flex shadow="md" rounded="xl" p="2" direction="column" >
-                      <Text color="gray.400">{comment.sender}</Text>
-                      <Text>
+                      <Text color="gray.400">@{comment.sender}</Text>
+                      <Text my="2">
                         {comment.comment}
                       </Text>
-                      <Box display="flex" m="2">
-                        <IconButton
-                          aria-label="Like"
-                          icon={<FaThumbsUp />}
-                          onClick={() => {setLiked(comment.id);setunLiked(-1)}}
-                          color={liked === comment.id ? 'red.300' : 'gray.400'}
-                          border="2px"
-                          borderColor='gray.300'
-                          borderRadius="full"
-                          bg="transparent"
-                          mx="2"
-                          _hover={{
-                            bg: 'gray.100',
-                          }}
-                          _active={{
-                            bg: 'gray.200',
-                          }}
-                        />
-                        <IconButton
-                          aria-label="unLike"
-                          icon={<FaThumbsDown />}
-                          onClick={() => {setunLiked(comment.id);setLiked(-1)}}
-                          color={unliked === comment.id  ? 'red.300' : 'gray.400'}
-                          border="2px"
-                          borderColor='gray.300'
-                          borderRadius="full"
-                          bg="transparent"
-                          _hover={{
-                            bg: 'gray.100',
-                          }}
-                          _active={{
-                            bg: 'gray.200',
-                          }}
-                        />
-                      </Box>
+                      {toggle === comment.id &&
+                        <Flex p="2" bg="white" shadow="md" rounded="full" position="absolute" ml={comment.sender_id === Number(profile.id) ? "-40" : "24"} >
+                          {[<FaThumbsUp />, <FaThumbsDown />, <FaHeart />, <FaLightbulb />].map((icon,index) => {
+                            return (
+                              <IconButton
+                                key={index}
+                                aria-label="Like"
+                                title="like"
+                                icon={icon}
+                                onClick={() => { handleIcon(icon) }}
+                                color='gray.300'
+                                bg="blue.300"
+                                border="2px"
+                                borderColor='gray.300'
+                                borderRadius="full"
+                                mx="2"
+                                _hover={{
+                                  bg: 'gray.100',
+                                }}
+                                _active={{
+                                  bg: 'gray.200',
+                                }}
+                              />
+                            )
+                          })}
+                        </Flex>
+                      }
+                      <Text color="gray.400" cursor="pointer" onClick={() => setToggle(comment.id)}>
+                        {icon !== null && toggle === comment.id * 10 ?
+                          <IconButton
+                            icon={icon}
+                            color='gray.400'
+                            border="2px"
+                            borderColor='gray.300'
+                            borderRadius="full"
+                            bg="blue.800"
+                            mx="2"
+                            _hover={{
+                              bg: 'gray.100',
+                            }}
+                            _active={{
+                              bg: 'gray.200',
+                            }}
+                          />
+
+                          : "Like"}
+                      </Text>
                     </Flex>
                   </Flex>
                 )
@@ -215,7 +220,7 @@ export default function ReadBlog() {
               <InputRightElement cursor="pointer" onClick={submitComment}>
                 <FaPaperPlane />
               </InputRightElement>
-              <Input placeholder='Type here...' value={comment} name="comment" onChange={handleComment} onKeyDown={handleKey} autoComplete="off" />
+              <Input placeholder='Type here...' value={comment} name="comment" onChange={(e) => setComment(e.target.value)} onKeyDown={handleKey} autoComplete="off" />
             </InputGroup>
           </DrawerFooter>
         </DrawerContent>
